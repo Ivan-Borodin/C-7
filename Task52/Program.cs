84fc34f63a81a8667332e31884e6ec9dfe66af75
Ivan <ivan241578@gmail.com>
2022-06-05 18:52:18 +0300

    Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Введите кол-во строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Random rnd = new Random();
+double sum = 0;
+double avg = 0;
+void FillArray(int[,] collection)
+{
+    for (int i = 0; i < collection.GetLength(0); i++)
+    {
+        for (int j = 0; j < collection.GetLength(1); j++)
+        {
+            collection[i, j] = rnd.Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] col)
+{
+    for (int i = 0; i < col.GetLength(0); i++)
+    {
+        for (int j = 0; j < col.GetLength(1); j++)
+        {
+            Console.Write($"{col[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Average(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            sum = sum + arr[j, i];
+        }
+        avg = sum / arr.GetLength(0);
+        Console.Write($"{Math.Round(avg, 1)} ");
+        sum = 0;
+    }
+}
+FillArray(array);
+PrintArray(array);
+Average(array);
